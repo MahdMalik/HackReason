43d41ed201748fbe24d82ae8bfc2524d2b6b4304
MahdMalik <mahdmahdmalik@gmail.com>
2025-01-16 18:35:56 -0600

    added python

--- app/page.js
+++ app/page.js
@@ -1,8 +1,30 @@
+'use client'
 import Image from "next/image";
-import styles from "./page.module.css";
+
+import { Typography, Container, AppBar, Toolbar, Button, Box, Grid } from '@mui/material'
 
 export default function Home() {
+  async function sendQuery() {
+    let query = "autism([social_emotional_deficits, non_verbal_comm_deficits, rel_maintenance_deficits, motor_stereotypes, hyper_hyporeactivity])."
+    try
+    {
+      const returnedValues = await fetch('http://127.0.0.1:5000/backend', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        body: JSON.stringify(query)
+      })
+      console.log(await returnedValues.json())
+    }
+    catch(e)
+    {
+      console.log("Failed to contact python. Error: " + e)
+    }
+    
+  }
   return (
-    <p>HELLO CHAT</p>
+    <Box>
+      <p>HELLO CHAT</p>
+      <Button onClick={sendQuery}>Click To Send</Button>
+    </Box>
   );
 }
